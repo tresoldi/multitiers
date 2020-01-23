8ba9544b7ceb5dbdc712aa8403d5b398e5961ea5
Tiago Tresoldi <tresoldi@shh.mpg.de>
2020-01-23 17:10:40 +0100

    More tests

--- tests/test_multitiers.py
+++ tests/test_multitiers.py
@@ -25,3 +25,33 @@ TEST_DATA = [
 class TestMultiTiers(unittest.TestCase):
     def test_multitiers(self):
         mt = multitiers.MultiTiers(TEST_DATA)
+
+    def test_multitiers_colnames(self):
+        mt = multitiers.MultiTiers(
+            TEST_DATA, doculect="DOCULECT", cogid="COGID", alignment="ALIGNMENT"
+        )
+
+    def test_multitiers_shifted(self):
+        mt = multitiers.MultiTiers(TEST_DATA, left=2, right=2)
+
+    def test_multitiers_str(self):
+        mt = multitiers.MultiTiers(TEST_DATA)
+        str(mt)
+
+    def test_multitiers_repr(self):
+        mt = multitiers.MultiTiers(TEST_DATA)
+        repr(mt)
+
+    def test_multitiers_hash(self):
+        mt1 = multitiers.MultiTiers(TEST_DATA)
+        mt2 = multitiers.MultiTiers(TEST_DATA)
+        mt3 = multitiers.MultiTiers(TEST_DATA[:-1])
+
+        assert hash(mt1) == hash(mt2)
+        assert hash(mt1) != hash(mt3)
+
+
+if __name__ == "__main__":
+    # Explicitly creating and running a test suite allows to profile
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestMultiTiers)
+    unittest.TextTestRunner(verbosity=2).run(suite)
