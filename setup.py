7ef6b76e56317ee7148bd03d3947c5c48e1befef
Tiago Tresoldi <tresoldi@shh.mpg.de>
2020-06-22 10:34:23 +0200

    Refactor setup.py, also adding requirements.txt

--- setup.py
+++ setup.py
@@ -7,30 +7,34 @@ LOCAL_PATH = pathlib.Path(__file__).parent
 # The text of the README file
 README_FILE = (LOCAL_PATH / "README.md").read_text()
 
+# Load requirements, so they are listed in a single place
+with open("requirements.txt") as fp:
+    install_requires = [dep.strip() for dep in fp.readlines()]
+
 # This call to setup() does all the work
 setup(
-    name="multitiers",
-    version="0.1",
-    description="A library for multi-tiered sequence representation of linguistic data.",
-    long_description=README_FILE,
-    long_description_content_type="text/markdown",
-    url="https://github.com/tresoldi/multitiers",
-    project_urls={"Documentation": "https://multitiers.readthedocs.io"},
-    author="Tiago Tresoldi",
     author_email="tresoldi@shh.mpg.de",
-    license="MIT",
+    author="Tiago Tresoldi",
     classifiers=[
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
         "Topic :: Software Development :: Libraries",
     ],
-    packages=["multitiers", "clts-master"],
-    keywords=["tiers", "multitiers", "phonology", "historical linguistics"],
-    include_package_data=True,
-    install_requires=["pyclts", "tabulate", "numpy", "pandas"],
+    description="A library for multi-tiered sequence representation of linguistic data.",
     entry_points={"console_scripts": ["multitiers=multitiers.__main__:main"]},
+    include_package_data=True,
+    install_requires=install_requires,
+    keywords=["tiers", "multitiers", "phonology", "historical linguistics"],
+    license="MIT",
+    long_description_content_type="text/markdown",
+    long_description=README_FILE,
+    name="multitiers",
+    packages=["multitiers", "clts-master"],
+    project_urls={"Documentation": "https://multitiers.readthedocs.io"},
     test_suite="tests",
     tests_require=[],
+    url="https://github.com/tresoldi/multitiers",
+    version="0.1",
     zip_safe=False,
 )
